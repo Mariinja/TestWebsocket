cf33d00126372f0e0f46d86251a0bd4497e1b7dd
Mariinja <91197819+Mariinja@users.noreply.github.com>
2022-04-03 10:46:56 +0200

    first running websocket file

---
+++ testWebsocket/server/index.js
@@ -0,0 +1,20 @@
+const Websocket = require("ws");
+
+const wss = new Websocket.Server({port : 8082});
+
+wss.on("connection", ws =>{
+    console.log("New client connected");
+
+    ws.on("message", data =>{
+        console.log(`Client has sent us:  ${data}`);
+
+        ws.send("Server received data, thanks");
+    });
+
+    ws.on("close", () =>{
+        console.log("Client has disconnected");
+
+    });
+});
+
+
